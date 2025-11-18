561415774d5c42e9e3d0a2e9706295bf1f806a17
Daan Brugmans <daanbrugmans@gmail.com>
2025-11-18 11:25:00 +0100

    Added model from HuggingFace

--- assignments/Assignment1ISS/deep_model.py
+++ assignments/Assignment1ISS/deep_model.py
@@ -3,4 +3,9 @@ import transformers
 
 class DeepEmotionDetector:
     def __init__(self):
-        self.model = transformers.ViT
+        self.preprocessor = transformers.ViTImageProcessor.from_pretrained(
+            "google/vit-base-patch16-224-in21k"
+        )
+        self.model = transformers.ViTForImageClassification.from_pretrained(
+            "dima806/facial_emotions_image_detection"
+        )
