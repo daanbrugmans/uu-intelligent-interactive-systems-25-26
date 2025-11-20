cf7d006a519b70ea8354654389dfa0f005ce3902
Daan Brugmans <daanbrugmans@gmail.com>
2025-11-21 00:16:55 +0100

    Implemented and ran training a ViT on the dataset

--- assignments/Assignment1ISS/deep_model.py
+++ assignments/Assignment1ISS/deep_model.py
@@ -1,19 +1,21 @@
+
+import os
 from abc import ABC
 from pathlib import Path
 
+import pandas as pd
+import torch
 import transformers
 import datasets
 import peft
+from tqdm import tqdm
+from sklearn.metrics import accuracy_score
+
+import runtime
 
 
 class DeepEmotionClassifier(ABC):
     def __init__(self):
-        # self.preprocessor = transformers.ViTImageProcessor.from_pretrained(
-        #     "google/vit-base-patch16-224-in21k"
-        # )
-        # self.model = transformers.ViTForImageClassification.from_pretrained(
-        #     "dima806/facial_emotions_image_detection"
-        # )
         super.__init__()
 
         self.processor
@@ -44,7 +46,7 @@ class FinetunedViT(DeepEmotionClassifier):
                 num_labels=7,
                 ignore_mismatched_sizes=True,
             ),
-            path_to_finetuned_model,
+            path_to_finetuned_model
         )
 
 
@@ -63,6 +65,7 @@ def train_vit_for_emotion_classification(
 ) -> None:
     def prepare_dataset_for_training(dataset: datasets.Dataset) -> datasets.Dataset:
         input_features = vit_model.processor(dataset["image"], return_tensors="pt")
+        input_features["pixel_values"] = torch.squeeze(input_features["pixel_values"])
         input_features["labels"] = dataset["label"]
 
         return input_features
@@ -73,14 +76,14 @@ def train_vit_for_emotion_classification(
         lora_dropout=0.1,
         bias="none",
         inference_mode=False,
-        target_modules=["query", "key", "value"],
+        target_modules=["query", "key", "value", "classifier"],
     )
 
     training_args = transformers.TrainingArguments(
         output_dir=None,
         eval_strategy="epoch",
         num_train_epochs=5,
-        learning_rate=3e-6,
+        learning_rate=3e-4,
         weight_decay=0.02,
         lr_scheduler_type="cosine",
         per_device_train_batch_size=8,
@@ -91,15 +94,14 @@ def train_vit_for_emotion_classification(
     )
 
     lora_vit = peft.get_peft_model(vit_model.model, lora_config)
-    preprocessed_train = train.map(prepare_dataset_for_training, batched=True)
-    preprocessed_val = val.map(prepare_dataset_for_training, batched=True)
+    preprocessed_train = train.map(prepare_dataset_for_training, batched=False)
+    preprocessed_val = val.map(prepare_dataset_for_training, batched=False)
 
     trainer = transformers.Trainer(
         lora_vit,
         args=training_args,
         train_dataset=preprocessed_train,
-        eval_dataset=preprocessed_val,
-        tokenizer=vit_model.tokenizer,
+        eval_dataset=preprocessed_val
     )
 
     trainer.train()
@@ -108,6 +110,39 @@ def train_vit_for_emotion_classification(
     path_to_finetuned_model = str(Path(project_root, "finetuned_vit"))
 
     lora_vit.save_pretrained(path_to_finetuned_model)
+    
+
+def dima806_label_to_fer_label(dima806_label: int) -> int:
+    if dima806_label == 0:
+        return 5 # Sad
+    elif dima806_label == 1:
+        return 1 # Disgust
+    elif dima806_label == 2:
+        return 0 # Anger
+    elif dima806_label == 3:
+        return 4 # Neutral
+    elif dima806_label == 4:
+        return 2 # Fear
+    elif dima806_label == 5:
+        return 6 # Surprise
+    elif dima806_label == 6:
+        return 3 # Happy
+    
+def get_predictions(vit_model, dataset: datasets.Dataset, use_dima806_labels: bool = False) -> list:   
+    predictions = []
+    
+    for row in tqdm(dataset):        
+        pixel_values = vit_model.processor(row["image"], return_tensors="pt").to(device)
+        model_output = vit_model.model(**pixel_values)
+        emissions = model_output.logits
+        prediction = emissions.argmax(-1).item()
+        
+        if use_dima806_labels:
+            prediction = dima806_label_to_fer_label(prediction)
+        
+        predictions.append(prediction)
+        
+    return predictions
 
 
 if __name__ == "__main__":
